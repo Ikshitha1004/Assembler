7d9163d45fae18692c513e0ae2209e1224d038c7
Ikshitha J <ikshi.thaj0477@gmail.com>
2025-10-27 16:48:43 +0530

    code.asm

--- tests/code.asm
+++ tests/code.asm
@@ -1,23 +1,55 @@
-.text
-.method add
-.limit stack 2
-.limit locals 3
-L0:
+.class_metadata
+class_count 1
+class_begin TestClass -1
+field_count 1
+field publicVar int 0
+method_count 2
+method TestClass TestClass.TestClass
+method TestClass TestClass.TestClass@int
+class_end
+.end_metadata
+
+.code 
+.method TestClass.TestClass
+.limit stack 4
+.limit locals 2
+PUSH 0
+STORE 1 ; Init i
+PUSH 7
+STORE 1 ; Store to local 'i'
+JMP L0
+L1:
+LOAD_ARG 0 ; 'this' for assignment to member 'publicVar'
+LOAD 0 ; Load 'this' to access member 'publicVar'
+GETFIELD 0
+LOAD 1  ; Load local var i
+IADD
+PUTFIELD 0
+L2:
+LOAD 1 ; Load current value of i for post increment
+DUP
 PUSH 1
-STORE 0 ; Store to a
-LOAD 0  ; Load a
-LOAD 1  ; Load b
 IADD
-STORE 0 ; Store to a
-PUSH 2
-STORE 2 ; Init c
-LOAD 0  ; Load a
-LOAD 1  ; Load b
-IADD
-LOAD 2  ; Load c
-IADD
+STORE 1 ; Post increment
+L0:
+LOAD 1  ; Load local var i
+PUSH 10
+ICMP_LT
+JNZ L1
+JMP L3
+L3:
 RET
 .endmethod
+
+.method TestClass.TestClass@int
+.limit stack 4
+.limit locals 2
+LOAD_ARG 0 ; 'this' for assignment to member 'publicVar'
+LOAD_ARG 1  ; Load parameter 'x'
+PUTFIELD 0
+RET
+.endmethod
+
 
 
 
